737b2078c0941c17db6292a6030e58d19d6024f9
Like <like@ekil.sh>
2026-02-01 08:34:07 +0800

    fix: only enable im-select input method switching on macOS

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -18,36 +18,38 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'CursorHold' }, {
   command = 'checktime',
 })
 
--- Auto switch to English input method (ABC) in Normal mode
-local im_english = 'com.apple.keylayout.ABC'
-local im_prev = im_english
+-- Auto switch to English input method (ABC) in Normal mode (macOS only)
+if vim.fn.has('macunix') == 1 then
+  local im_english = 'com.apple.keylayout.ABC'
+  local im_prev = im_english
 
-vim.api.nvim_create_autocmd('InsertLeave', {
-  desc = 'Switch to English input method when leaving Insert mode',
-  group = vim.api.nvim_create_augroup('custom-im-switch', { clear = true }),
-  callback = function()
-    im_prev = vim.fn.system('im-select'):gsub('%s+', '')
-    vim.fn.jobstart({ 'im-select', im_english }, { detach = true })
-  end,
-})
-
-vim.api.nvim_create_autocmd('InsertEnter', {
-  desc = 'Restore previous input method when entering Insert mode',
-  group = vim.api.nvim_create_augroup('custom-im-restore', { clear = true }),
-  callback = function()
-    vim.fn.jobstart({ 'im-select', im_prev }, { detach = true })
-  end,
-})
-
-vim.api.nvim_create_autocmd('FocusGained', {
-  desc = 'Switch to English input method when Neovim gains focus',
-  group = vim.api.nvim_create_augroup('custom-im-focus', { clear = true }),
-  callback = function()
-    if vim.fn.mode() ~= 'i' then
+  vim.api.nvim_create_autocmd('InsertLeave', {
+    desc = 'Switch to English input method when leaving Insert mode',
+    group = vim.api.nvim_create_augroup('custom-im-switch', { clear = true }),
+    callback = function()
+      im_prev = vim.fn.system('im-select'):gsub('%s+', '')
       vim.fn.jobstart({ 'im-select', im_english }, { detach = true })
-    end
-  end,
-})
+    end,
+  })
+
+  vim.api.nvim_create_autocmd('InsertEnter', {
+    desc = 'Restore previous input method when entering Insert mode',
+    group = vim.api.nvim_create_augroup('custom-im-restore', { clear = true }),
+    callback = function()
+      vim.fn.jobstart({ 'im-select', im_prev }, { detach = true })
+    end,
+  })
+
+  vim.api.nvim_create_autocmd('FocusGained', {
+    desc = 'Switch to English input method when Neovim gains focus',
+    group = vim.api.nvim_create_augroup('custom-im-focus', { clear = true }),
+    callback = function()
+      if vim.fn.mode() ~= 'i' then
+        vim.fn.jobstart({ 'im-select', im_english }, { detach = true })
+      end
+    end,
+  })
+end
 
 -- Auto restore session on startup (only when no args passed)
 vim.api.nvim_create_autocmd('VimEnter', {
